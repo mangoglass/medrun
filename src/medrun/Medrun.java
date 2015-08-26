872b868efa714fd2ed90c45a29ed4fa4e56095f7
Axblad <axblad@kth.se>
2015-08-26 11:32:17 +0200

    removed small wierd code bug.

--- src/medrun/Medrun.java
+++ src/medrun/Medrun.java
@@ -124,11 +124,7 @@ public class Medrun extends StateBasedGame {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws SlickException, FileNotFoundException, IOException {
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> master
         game = new Medrun("Medrun");
         app = new AppGameContainer(new ScalableGame(game, width, height, false));
         try{
